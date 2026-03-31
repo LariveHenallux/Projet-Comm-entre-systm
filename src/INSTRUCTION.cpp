46bf99fb94f17225c523af620be7f7c30094e41d
AlexisGillieaux <etu55085@student.henallux.be>
2026-03-31 14:30:09 +0200

    changement 5 en 50 (5*20 != 1000)

--- src/INSTRUCTION.cpp
+++ src/INSTRUCTION.cpp
@@ -1,6 +1,6 @@
 int * listInstruction(int temps, int degrefinal, int degreInit) {
-    int * result = new int[temps * 5];
-    int hertz = temps * 5;
+    int * result = new int[temps * 50];
+    int hertz = temps * 50;
     int step = (degrefinal - degreInit) / hertz; // Calculate the step size for each increment
     for (int i = 0; i < hertz; i++) {
         result[i] = degreInit + (step * i); // Initialize all elements to the corresponding step value
